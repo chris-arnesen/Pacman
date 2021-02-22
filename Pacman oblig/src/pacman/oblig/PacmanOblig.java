cbddb0a865a864a5bfe8a3f84e2d542085c176ce
🌝Jacob🌚 <Jacobkristensen@hotmail.no>
2021-02-22 13:36:24 +0100

    barmen

--- Pacman oblig/src/pacman/oblig/PacmanOblig.java
+++ Pacman oblig/src/pacman/oblig/PacmanOblig.java
@@ -1,4 +1,4 @@
-/*
+/* barmen
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -125,7 +125,6 @@ public class PacmanOblig extends Application {
                 String linje = leser.nextLine();
                 for (int i = 0; i < linje.length(); i++ ) {
                     char c = linje.charAt(i);
-                    System.out.print(c);
                     if ( c == 'x' ) {
                         Rectangle s = new Rectangle(pointerX, pointerY, 10,10);
                         center.getChildren().add(s);
