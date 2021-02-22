ca5c8fdc909a58f5c893d080d7c403381ef926e1
🌝Jacob🌚 <Jacobkristensen@hotmail.no>
2021-02-22 15:06:17 +0100

    la til egen fil for centerPane

---
+++ Pacman oblig/src/pacman/oblig/centerPane.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pacman.oblig;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+
+/**
+ *
+ * @author jacob
+ */
+public class centerPane extends Pane {
+    
+    
+    protected int height = (500/28)*31;
+    protected int STR = 500/28;
+    
+    
+    public centerPane() {
+        this.setPrefHeight(height);
+        this.setStyle("-fx-border-color: black; -fx-background-color: white;");
+        getMap();
+    }
+    
+    //Metode som leser inn kartet(banen)
+    public Scanner getMap() {
+             Scanner leser = null;
+             int pointerX = 0, pointerY = 0;
+             
+        try{
+          File fil = new File("pacman-kart.txt");
+            leser = new Scanner(fil);
+            while ( leser.hasNextLine() ) {
+                String linje = leser.nextLine();
+                for (int i = 0; i < linje.length(); i++ ) {
+                    char c = linje.charAt(i);
+                    if ( c == 'x' ) {
+                        Rectangle s = new Rectangle(pointerX, pointerY, STR,STR);
+                        this.getChildren().add(s);
+                        pointerX+=STR;
+                    } else if(c == '-'){
+                        Circle s = new Circle(pointerX+(10/2),pointerY+(10/2),2);
+                        this.getChildren().add(s);
+                        pointerX+=STR;
+                    } else
+                        pointerX+=STR;
+                        
+                        
+                } //Slutt på for-løkke
+                pointerY+=STR;
+                pointerX=0;
+            } //Slutt på while-løkke
+            leser.close();
+            
+        } catch (FileNotFoundException e) //Slutt på "try" fortsetter med "catch"
+        { 
+        System.out.println("fant ikke fil");}
+        
+        return leser;
+        } 
+    
+}
